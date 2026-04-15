42907e514983b50762d065d566b03a282ab80e87
Darshan PL <darshanpl1382006@gmail.com>
2026-04-16 00:09:02 +0530

    Create P6.py

---
+++ P6.py
@@ -0,0 +1,6 @@
+def average(arr):
+    # Calculate the total sum of elements in the array
+    total = sum_array(arr)
+    
+    # Divide the total by number of elements to get average
+    return total / len(arr)
