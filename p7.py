a1bfda80701f2e139d4b6ea23c8a100935d890e4
Darshan PL <darshanpl1382006@gmail.com>
2026-04-16 20:03:55 +0530

    Update p7.py

--- p7.py
+++ p7.py
@@ -1,1 +1,11 @@
-
+def count_even_odd(arr):
+    even = 0
+    odd = 0
+    
+    for num in arr:
+        if num % 2 == 0:
+            even += 1  # Count even
+        else:
+            odd += 1   # Count odd
+            
+    return even, odd
