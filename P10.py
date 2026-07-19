198755d96ae229e0e5b22f5b8caba23ef5c31c65
Darshan PL <darshanpl1382006@gmail.com>
2026-07-20 02:11:13 +0530

    Update P10.py

--- P10.py
+++ P10.py
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int key = 30;
+
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] == key) {
+            cout << "Found at index " << i;
+            return 0;
+        }
+    }
+
+    cout << "Not Found";
+}
